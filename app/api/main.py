4d72cbb77248f324a79bbcdf7f66a03dfdc311f7
narcotis <qhguswkd@gmail.com>
2021-11-15 22:08:25 +0900

    fastapi & nats & krakend

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,27 @@
+import asyncio
+import uvicorn
 from fastapi import FastAPI
+from typing import AsyncGenerator, Dict
+from nats.aio.client import Client as NATS
+from nats.aio.client import Msg
+from starlette.requests import Request
+from starlette.responses import StreamingResponse
+import nats
 
 app = FastAPI()
+nc = NATS()
+js = nc.jetstream()
+
+@app.on_event("startup")
+async def nats_connect():
+    global nc, js
+    if not nc.is_connected:
+        await nc.connect('nats://nats:4222')
+    await js.add_stream(name="msa-test", subjects=["api"])
+    psub = await js.pull_subscribe("api", "psub")
+
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
